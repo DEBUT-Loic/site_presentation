d0a85549ab3a98c4282d8fd0b4929291d5177dee
DEBUT Loïc <zebrane.safari@gmail.com>
2023-12-12 18:47:17 +0100

    Réparation des 2 sliders

--- script/slide.js
+++ script/slide.js
@@ -1,5 +1,4 @@
 $("header li").click(function() {
-	console.log($("div").eq(1).offset());
 	$("header li").css({background:"white",color:"black"});
 	$(this).css({background:"black",color:"white"});
 })
@@ -18,16 +17,26 @@ for(let i=0;i<$(".slider").length;i++) {
 		$(".apres").eq(i).remove();
 	}
 }
-let posSlider=[0,0], wSlide;
-wSlide=$(".slider").get(0).scrollWidth;
+let posSlider=[0,0], wSlide, finSlide=[0,0], totalSlides;
+finSlide[0]=-$(window).width();
+finSlide[1]=-$(window).width();
+
 $("#s3 > button, #s4 > button").click(function() {
 	let iAft=$(".apres").index($(this)), iBef=$(".avant").index($(this));
+
+	if($(this).parent().attr("id")=="s3") {
+		wSlide=$(".slider > div:first-child").get(0).scrollWidth*-1;
+		totalSlides=wSlide*$(".slider > .cardBox").length;
+	}
+	else {
+		wSlide=$(".slider > div:first-child").get(1).scrollWidth*-1;
+		totalSlides=wSlide*$(".slider > .card").length;
+	}
+
 	if($(this).hasClass("apres")) {
-		if(posSlider[iAft]<=-wSlide+$(window).width()) {
-			posSlider[iAft]=-wSlide+$(window).width();
-		}
-		else {
-			posSlider[iAft]-=$(window).width()/3;
+		if(finSlide[iAft]>totalSlides) {
+			posSlider[iAft]+=wSlide;
+			finSlide[iAft]+=wSlide;
 		}
 	}
 	else {
@@ -35,9 +44,11 @@ $("#s3 > button, #s4 > button").click(function() {
 			posSlider[iBef]=0;
 		}
 		else {
-			posSlider[iBef]+=$(window).width()/3;
+			posSlider[iBef]-=wSlide;
+			finSlide[iBef]-=wSlide;
 		}
 	}
+	
 	$(".slider").eq(iBef).css("transform",`translateX(${posSlider[iBef]}px)`);
 	$(".slider").eq(iAft).css("transform",`translateX(${posSlider[iAft]}px)`);
 })
